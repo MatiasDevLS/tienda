32f4ee155f3e8011e51e562207fe4db279834f73
Matias <matiaslopse@gmail.com>
2025-02-11 12:23:36 +0100

    Cambio_serviceExtensions

--- Api/Extensions/ApplicationServiceExtensions.cs
+++ Api/Extensions/ApplicationServiceExtensions.cs
@@ -1,19 +1,30 @@
 using Api.Data;
+using Api.Entities;
 using Api.Helpers;
 using Api.Interfaces;
 using Api.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Extensions
 {
     public static class ApplicationServiceExtensions
     {
+    
         public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddIdentityCore<Usuario>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
 
             services.AddCors();
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
